df5c0881646e4969095176addccce54d25c903a2
ITIzalco <it@izalco.gob.sv>
2023-06-23 11:25:04 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Nota;
+            int i = 1, Aprobados=0, Reprobados=0;
+            while (i <= 10 )
+            {
+                Console.WriteLine("Nota {0}:", i);
+                Nota = int.Parse(Console.ReadLine());
+
+                if (Nota > 7) 
+                {
+                    Aprobados++;
+                }
+                else
+                {
+                    Reprobados++;
+                }
+
+
+                i++; 
+
+
+            }
+            Console.WriteLine("Aprobados: " + Aprobados + " Reprobados :" + Reprobados);
+            Console.ReadLine();
+        }
+    }
+}
